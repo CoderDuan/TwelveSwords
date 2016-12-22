9f22bb6837e4b94beab177a418621bb3c8c8c7d3
coderduan <duanjingyao2011@163.com>
2016-12-22 14:33:54 +0800

    Added Creature, Hero, Monster, Item and Title class. And cleared useless files.

---
+++ project/TwelveSwords/Assets/Monster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Monster : Creature
+{
+    public uint turn;
+    public string description;
+
+    public Monster(string name, int hp, int mp, int atk, int def, int mag)
+        : base(name, hp, mp, atk, def, mag)
+    {
+        
+    }
+
+    public void takeTurn()
+    {
+
+    }
+}
